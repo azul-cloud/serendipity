9f809a8507ebbf54d008fdcc22c0cc190370d8f3
Adam Wester <awwester@gmail.com>
2015-06-20 08:27:38 -0500

    fix calendar|

--- fullcalendar/fullcalendar.py
+++ fullcalendar/fullcalendar.py
@@ -4,11 +4,11 @@ from django.conf import settings
 # folder structure, expected to be under the STATIC_URL
 
 FULLCALENDAR_DEFAULTS = {
-    'css_url': '//cdnjs.cloudflare.com/ajax/libs/fullcalendar/1.6.4/fullcalendar.css',
-    'print_css_url': '//cdnjs.cloudflare.com/ajax/libs/fullcalendar/1.6.4/fullcalendar.print.css',
-    'javascript_url': '//cdnjs.cloudflare.com/ajax/libs/fullcalendar/1.6.4/fullcalendar.min.js',
-    'jquery_url': '//code.jquery.com/jquery-2.1.0.min.js',
-    'jquery_ui_url': '//code.jquery.com/ui/1.10.4/jquery-ui.js',
+    'css_url': '//cdnjs.cloudflare.com/ajax/libs/fullcalendar/2.3.2/fullcalendar.min.css',
+    'print_css_url': '//cdnjs.cloudflare.com/ajax/libs/fullcalendar/2.3.2/fullcalendar.print.css',
+    'javascript_url': '//cdnjs.cloudflare.com/ajax/libs/fullcalendar/2.3.2/fullcalendar.min.js',
+    # 'jquery_url': '//code.jquery.com/jquery-2.1.0.min.js',
+    'jquery_ui_url': '//fullcalendar.io/js/fullcalendar-2.1.1/lib/jquery-ui.custom.min.js',
 }
 
 # Updates location based on configuration defined by 
